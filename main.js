164c8ddd05e2e82b28c8be42a8479020858f0b88
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2020-11-13 09:39:40 -0700

    gather selected number of images

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 function getDogImages() {
     console.log('getting images');
-    fetch('https://dog.ceo/api/breeds/image/random/3')
+    fetch('https://dog.ceo/api/breeds/image/random/'+numberOfImages())
       .then(response => response.json())
-      .then(responseJson => console.log(responseJson));
+      .then(responseJson => displayImages(responseJson));
       /*.then(responseJson => displayResults(responseJson))
       .catch(error => alert('Something went wrong. Try again later.'));
       console.log($(responseJson));*/
   }
 
+  function numberOfImages(){
+     let number = document.getElementById('numDogs').value;
+     console.log(number);
+     return number;
+  }
+
+  function displayImages() {
+    console.log(resultsJson);
+    
+  }
+
   function userSubmit() {
-    $('form').on('click', '#submit', event => {
+    $('form').submit( event => {
         console.log('submitting');
       event.preventDefault();
       getDogImages();
     });
   }
 
+function runDogApp() {
+    userSubmit();
+}
+
+  $(runDogApp());
